ee878d26d228720fc93b09f1cdeccd919d80ef54
G. Santos <49494916+devgsantos@users.noreply.github.com>
2026-07-05 12:12:31 -0300

    Mark thread-local cycle budget diagnostics

--- src/switch/switch_jit.cpp
+++ src/switch/switch_jit.cpp
@@ -57,7 +57,7 @@ bool LogResultFailure(const char* operation, Result result) {
 } // namespace
 
 bool RunJitSelfTest() {
-    AppendJitLog("diagnostics_version=10 concrete_host_timing_bridges=enabled thread_local_breadcrumbs=enabled");
+    AppendJitLog("diagnostics_version=12 thread_local_cycle_budget=enabled thread_local_breadcrumbs=enabled");
     AppendJitLog("Switch JIT self-test enter");
 
     Jit jit{};
