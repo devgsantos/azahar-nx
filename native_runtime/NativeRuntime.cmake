0f99fa5a4640a3f3ca9324d62515267fee7806c6
G. Santos <49494916+devgsantos@users.noreply.github.com>
2026-07-07 22:40:51 -0300

    Use the direct native runtime configuration

--- native_runtime/NativeRuntime.cmake
+++ native_runtime/NativeRuntime.cmake
@@ -1,16 +1,1 @@
-function(azahar_enable_native_runtime)
-    if(TARGET video_core)
-        target_compile_definitions(video_core PRIVATE
-            AZAHAR_DEKO3D_STRICT_NATIVE
-            AZAHAR_DEKO3D_NATIVE_RUNTIME)
-        target_precompile_headers(video_core PRIVATE
-            "$<$<COMPILE_LANGUAGE:CXX>:${CMAKE_SOURCE_DIR}/src/video_core/renderer_deko3d/deko3d_native_policy.h>")
-    endif()
-
-    if(TARGET citra_core)
-        target_compile_definitions(citra_core PRIVATE
-            AZAHAR_SWITCH_DYNARMIC_SAFE_OPTIMIZATIONS)
-    endif()
-endfunction()
-
-cmake_language(DEFER CALL azahar_enable_native_runtime)
+include("${CMAKE_CURRENT_LIST_DIR}/NativeRuntimeFinal.cmake")
