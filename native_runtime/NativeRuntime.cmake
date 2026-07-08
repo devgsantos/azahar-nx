b58ef2e3539caeab3cf34abb98e56cd35add49f6
G. Santos <49494916+devgsantos@users.noreply.github.com>
2026-07-07 21:38:32 -0300

    Add native runtime CMake entrypoint

---
+++ native_runtime/NativeRuntime.cmake
@@ -0,0 +1,4 @@
+# Load this file with CMAKE_PROJECT_INCLUDE when configuring the Switch build.
+include("${CMAKE_CURRENT_LIST_DIR}/NativeRuntimeDefinitions.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/NativeRuntimeState.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/NativeRuntimeQueue.cmake")
