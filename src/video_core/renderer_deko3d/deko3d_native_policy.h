e1c15cc03cb9933555a90cfb59ef27a955c39cc9
G. Santos <49494916+devgsantos@users.noreply.github.com>
2026-07-07 22:39:59 -0300

    Remove obsolete native policy overlay

--- src/video_core/renderer_deko3d/deko3d_native_policy.h
+++
@@ -1,5 +0,0 @@
-// Unified native Deko3D precompiled-header policy.
-#pragma once
-
-#include "video_core/renderer_deko3d/deko3d_native_overrides.h"
-#include "video_core/renderer_deko3d/deko3d_native_rasterizer_override.h"
