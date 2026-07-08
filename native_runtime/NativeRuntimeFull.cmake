0d4f6047ae5a85e0f71ba97942f832aaed19a2c3
G. Santos <49494916+devgsantos@users.noreply.github.com>
2026-07-07 21:39:02 -0300

    Add complete native runtime CMake entrypoint

---
+++ native_runtime/NativeRuntimeFull.cmake
@@ -0,0 +1,4 @@
+include("${CMAKE_CURRENT_LIST_DIR}/NativeRuntimeDefinitions.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/NativeRuntimeState.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/NativeRuntimeQueue.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/NativeRuntimeSupport.cmake")
